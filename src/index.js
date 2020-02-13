47c029530bbffdc82c7a36355009a6279cd6c96c
Andrea Rocca <andrea.rocca3@gmail.com>
2020-02-13 11:36:03 +0100

    stuff

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const accessToken = '01e8d52b07eddb1c34109ab5b9153bb4000e14d1'
+const baseUrl = 'https://api.github.com/user/repos'
+
+// Let's build a GitHub clone
+
+// Pseudo-code
+// 1. fetch request to api to pull data
+fetch(baseUrl, {
+  headers: {
+    authorization: `token ${accessToken}`
+  }
+})
+  .then(response => response.json())
+  .then(data =>{
+    const repos = document.querySelector('#repos')
+    data.forEach((element) => {
+    repos.insertAdjacentHTML('beforeend', `<a class="repo">${element.name}</a>`)
+    })
+    console.log(data)
+  })
+// 2. select element in html
+// 3. add data to html
+// 4. select input
+  const input = document.querySelector('#repo-name')
+  const button = document.querySelector('#repo-btn')
+// 5. add eventListner to the button
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+    fetch(baseUrl, {
+    headers: {
+    authorization: `token ${accessToken}`
+    },
+    method: 'POST',
+    body: JSON.stringify({name: input.value})
+    })
+  .then(response => response.json())
+  .then(data =>{
+    alert('Repos created')
+    console.log(data)
+  })
+  })
+// 6. make a post request to the github API
+
+// 7. Alert
